3f02bab5b25c465e9100e0078605bb4094ead81b
Chanond Wongpiya <chanond@salweensolutions.com>
2017-06-30 17:21:51 +0700

    Add customer service (part 8)

---
+++ Services/CustomerService.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Orchard.ContentManagement;
+using Orchard.Security;
+using Orchard.Services;
+using Orchard.Users.Models;
+using Orchard.Webshop.Models;
+
+namespace Orchard.Webshop.Services
+{
+    public class CustomerService : ICustomerService
+    {
+        private readonly IOrchardServices _orchardServices;
+        private readonly IMembershipService _membershipService;
+        private readonly IClock _clock;
+
+        public CustomerService(IOrchardServices orchardServices, IMembershipService membershipService, IClock clock)
+        {
+            _orchardServices = orchardServices;
+            _membershipService = membershipService;
+            _clock = clock;
+        }
+
+        public CustomerPart CreateCustomer(string email, string password)
+        {
+            // New up a new content item of type "Customer"
+            var customer = _orchardServices.ContentManager.New("Customer");
+
+            // Cast the customer to a UserPart
+            var userPart = customer.As<UserPart>();
+
+            // Cast the customer to a CustomerPart
+            var customerPart = customer.As<CustomerPart>();
+
+            // Set some properties of the customer content item (via UserPart and CustomerPart)
+            userPart.UserName = email;
+            userPart.Email = email;
+            userPart.NormalizedUserName = email.ToLowerInvariant();
+            userPart.Record.HashAlgorithm = "SHA1";
+            userPart.Record.RegistrationStatus = UserStatus.Approved;
+            userPart.Record.EmailStatus = UserStatus.Approved;
+
+            // Use IClock to get to the current date instead of using DateTime.Now
+            customerPart.CreatedUtc = _clock.UtcNow;
+
+            // Use Orchard's MembershipService to set the password of our new user
+            _membershipService.SetPassword(userPart, password);
+
+            // Store the new user into the database
+            _orchardServices.ContentManager.Create(customer);
+
+            return customerPart;
+        }
+    }
+}
---
+++ Services/ICustomerService.cs
@@ -0,0 +1,9 @@
+﻿using Orchard.Webshop.Models;
+
+namespace Orchard.Webshop.Services
+{
+    public interface ICustomerService : IDependency
+    {
+        CustomerPart CreateCustomer(string email, string password);
+    }
+}
